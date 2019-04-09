d247c67559408df1d86b34bea274b070b069849d
sukantahui <huisukanta77@gmail.com>
2019-04-09 21:49:25 +0530

    update table with transactionBegin, commit and rollback, database script

--- database_script/restful_db.sql
+++ database_script/restful_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 09, 2019 at 01:14 AM
+-- Generation Time: Apr 09, 2019 at 09:18 AM
 -- Server version: 5.7.19
 -- PHP Version: 7.1.8
 
@@ -31,21 +31,30 @@ SET time_zone = "+00:00";
 CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
   `product_name` varchar(200) DEFAULT NULL,
-  `product_price` int(11) DEFAULT NULL
+  `product_price` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`product_id`, `product_name`, `product_price`) VALUES
-(1, 'Product 1', 2000),
-(2, 'Product 2', 5000),
-(3, 'Product 3', 4000),
-(4, 'Product 4', 6000),
-(5, 'Product 5', 7000),
-(6, NULL, NULL),
-(7, 'Product 6 Added', 6000);
+INSERT INTO `product` (`product_id`, `product_name`, `product_price`, `created_at`) VALUES
+(1, 'Product 1', 2000, '2019-04-09 15:45:31'),
+(2, 'Product 2', 5000, '2019-04-09 15:45:31'),
+(3, 'Product 3', 4000, '2019-04-09 15:45:31'),
+(4, 'Product 4', 6000, '2019-04-09 15:45:31'),
+(5, 'Product 5', 7000, '2019-04-09 15:45:31'),
+(6, NULL, NULL, '2019-04-09 15:45:31'),
+(7, 'Product 6 Added', 6000, '2019-04-09 15:45:31'),
+(9, 'Product 7 Added', 6000, '2019-04-09 15:45:31'),
+(10, 'Product 7 Added', 6000, '2019-04-09 15:45:31'),
+(11, 'Product 7 Added', 6000, '2019-04-09 15:45:31'),
+(12, 'Product 7 Added', 6000, '2019-04-09 15:45:31'),
+(13, 'Product 31 Added', 6000, '2019-04-09 15:45:31'),
+(14, NULL, NULL, '2019-04-09 15:54:00'),
+(15, 'Product 31 Added', 6000, '2019-04-09 15:57:17'),
+(16, 'Product 41 Added', 589, '2019-04-09 16:15:26');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +74,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;COMMIT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
